e21b02d801dd9e81d8881506bdccdb4b47b92893
nana <106505931+na-hyeong9@users.noreply.github.com>
2022-09-08 02:27:07 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,12 @@
+N = int(input())
+group = N
+
+for _ in range(N):
+    word = input()
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            continue
+        elif word[i] in word[i+1:]:
+            group -= 1
+            break
+print(group)
