479f51aba7d52f7a623587e98f279c3fa4f14898
nana <106505931+na-hyeong9@users.noreply.github.com>
2023-01-13 16:11:03 +0900

    [Silver V] Title: 소수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -4,13 +4,14 @@ n = int(input())
 li = []
 for i in range(m, n+1):
     cnt = 0
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                cnt += 1
-                break  
-        if cnt == 0:
-            li.append(i) 
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            cnt += 1
+            break 
+    if cnt == 0:
+        li.append(i) 
 
 if len(li) != 0:
     print(sum(li))
