4dd5859e55db57ca57de57b17ec20d4571a743c1
nana <106505931+na-hyeong9@users.noreply.github.com>
2023-03-18 23:56:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,28 @@
+def solution(X, Y):
+    answer  = []
+    xDict   = dict()
+    yDict   = dict()
+    
+    # xDict 생성
+    for x in X:
+        xDict[x] = xDict.get(x,0)+1
+        
+    # yDict 생성
+    for y in Y:
+        yDict[y] = yDict.get(y,0)+1
+    
+    # xDict의 key를 기준으로 탐색해서, 짝꿍이 있다면, xDict와yDict에서 -1 해주고 answer에 추가
+    for k,v in xDict.items():
+        if k in yDict.keys():
+            while yDict[k]>0 and xDict[k]>0:
+                answer.append(k)
+                yDict[k]=yDict.get(k)-1
+                xDict[k]=xDict.get(k)-1
+    
+    if(len(answer)==0):                 return "-1"   
+
+    if(answer.count('0')==len(answer)): return "0"
+
+    answer.sort(reverse=True)
+    
+    return ''.join(answer)
