3a493713e6697e98c81bcb5cae0d2255d30d4143
nana <106505931+na-hyeong9@users.noreply.github.com>
2023-03-23 18:30:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 96.85 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    
+    for i in range(1, len(arr)):  
+        if arr[i-1] != arr[i]:
+            answer.append(arr[i])
+
+    return answer
